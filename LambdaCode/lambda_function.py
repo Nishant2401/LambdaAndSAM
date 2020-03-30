5e1f4048271382332cab1c18d2c67e10c1a9c916
Nishant Jain <nishantjain@GTT-MacBook-Pro.local>
2020-03-30 12:02:43 -0500

    Test trigger - change in Lambda code

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -12,7 +12,6 @@ def lambda_handler(event, context):
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
 
-# Sample change
 # import boto3
 # import json
 
