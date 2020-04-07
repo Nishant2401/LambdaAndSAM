63c45eb9a9e65691aec6787fae0b80810d35d324
Nishant Jain <nishantjain@GTT-MacBook-Pro.local>
2020-04-07 13:34:36 -0500

    Wrong Change

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -4,15 +4,16 @@ def lambda_handler(event, context):
     # TODO implement 
     print(event)
     data = {
-        'output': 'Hello from '+ event['Country'],
-        #'output': 'Hello from UK',
+        #'output': 'Hello from '+ event['Country'],
+        'output': 'Hello from UK',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
 
-# import boto3
+#Some change
+# # import boto3
 # import json
 
 # ec2 = boto3.client('ec2')
